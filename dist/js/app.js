66bf57f823fc2e3c8092bcacc94eff6f6b5673f4
RamonAPereira <ramonpereiranzd@gmail.com>
2021-09-04 22:26:56 -0300

    teste

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,7 @@
+let UInavigationMenu = document.querySelector('.navigation-menu');
 
 function menuActive() {
-  let UInavigationMenu = document.querySelector('.navigation-menu');
-  if(UInavigationMenu.style.display === 'none'){
+  if(window.getComputedStyle(UInavigationMenu).display === 'none'){
     UInavigationMenu.style.display = 'flex';
   }else{
     UInavigationMenu.style.display = 'none';
